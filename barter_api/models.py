d6b8939c445ec97dcbfe990b3731ac10c775c240
coIorbIind <labinfpin13@gmail.com>
2022-09-18 18:05:00 +0300

    Barter
    Create model for barter products

--- barter_api/models.py
+++ barter_api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from hostel_api.models import Student
 
-# Create your models here.
+
+class Product(models.Model):
+    """Модель товара"""
+    author = models.ForeignKey(Student, verbose_name='Автор объявления', on_delete=models.CASCADE)
+    title = models.CharField(verbose_name='Заголовок', max_length=50)
+    description = models.TextField(verbose_name='Описание', max_length=1000)
+    price = models.PositiveIntegerField(verbose_name='Цена', null=True)
+    creation_date = models.DateTimeField(verbose_name='Дата создания', auto_now_add=True)
+    is_published = models.BooleanField(verbose_name='Опубликовано', default=True)
+
+    def __str__(self):
+        return f'{self.title} ({self.author.surname})'
